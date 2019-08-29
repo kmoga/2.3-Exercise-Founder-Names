e0030ff7b8a60774d0413a479273bbb5ac3e15a6
Kyle Moga <mogak2016@gmail.com>
2019-08-29 16:10:22 -0700

    Quiz 1 changes

--- 2.3 Exercise Founder Names/ViewController.swift
+++ 2.3 Exercise Founder Names/ViewController.swift
@@ -9,12 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var founder1: UILabel!
+    
+    @IBOutlet weak var founder2: UILabel!
+    
+    var founders = ["Sergey Brin","Larry Page","Jennifer Hyman","Jenny Fleiss"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func bGoogle(_ sender: UIButton) {
+        founder1.text = founders[0]
+        founder2.text = founders[1]
+        
+    }
+    
+    @IBAction func bRunway(_ sender: UIButton) {
+        founder1.text = founders[2]
+        founder2.text = founders[3]
+        
+    }
+    
+    @IBAction func clear(_ sender: UIButton) {
+        founder1.text = ""
+        founder2.text = ""
+    }
+    
 }
 
