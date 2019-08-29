e713fe97226fde6b68ed12a8f4a7913c2e5af92d
Kyle Moga <mogak2016@gmail.com>
2019-08-29 15:16:52 -0700

    Initial Commit

---
+++ 2.3 Exercise Founder Names/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  2.3 Exercise Founder Names
+//
+//  Created by Kyle Moga on 8/29/19.
+//  Copyright © 2019 Kyle Moga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
